d7ef69cebf3d5ab308e943960397bbaf7d535e13
Ramin Bakhshpour <ramin.bpr@gmail.com>
2016-12-18 19:54:34 +0330

    node class

--- src/com/rb/megen/Node.java
+++ src/com/rb/megen/Node.java
@@ -1,4 +1,38 @@
 package com.rb.megen;
 
 public class Node {
+    private String data;
+    private Node left;
+    private Node right;
+
+    public String getData() {
+        return data;
+    }
+
+    public void setData(String data) {
+        this.data = data;
+    }
+
+    public Node getLeft() {
+        return left;
+    }
+
+    public void setLeft(Node left) {
+        this.left = left;
+    }
+
+    public Node getRight() {
+        return right;
+    }
+
+    public void setRight(Node right) {
+        this.right = right;
+    }
+
+
+    public Node (String data) {
+        this.data = data;
+        left = null;
+        right= null;
+    }
 }
